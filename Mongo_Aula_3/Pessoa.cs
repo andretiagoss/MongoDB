2083740873d27fd5b6a9f1ad8f2382870a51b099
Andre Tiago dos Santos Silva <andretiagoss@gmail.com>
2017-07-02 23:09:08 -0300

    primeito projeto

---
+++ Mongo_Aula_3/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace Mongo_Aula_3
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public List<string> Treinamentos { get; set; }
+    }
+
+    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreExtraElements] //ignora propriedades caso a mesma não exista mais na entidade
+    public class Pessoa2
+    {
+        [BsonId] //Define o campo código como BsonId.
+        public int Codigo { get; set; }
+
+        [MongoDB.Bson.Serialization.Attributes.BsonElement("Observacao")] //renomeia o nome
+        [MongoDB.Bson.Serialization.Attributes.BsonIgnoreIfNull] //inora a propriedade caso seja nula
+        public string Obs { get; set; }
+        [MongoDB.Bson.Serialization.Attributes.BsonRepresentation(BsonType.Double)] //converte para double
+        public decimal Salario { get; set; }
+
+        [MongoDB.Bson.Serialization.Attributes.BsonDateTimeOptions(Kind =DateTimeKind.Local)] //converte a data no formato local
+        public DateTime DataInsercao { get; set; }
+
+        [BsonIgnoreIfDefault] //ignora a propriedade quando a mesma conter valor default
+        public int Contador { get; set; }
+
+        [BsonDefaultValue("Casado")]
+        public string EstadoCivil { get; set; }
+
+    }
+}
---
+++ Mongo_Aula_3/Program.cs
@@ -0,0 +1,151 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongo_Aula_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DocumentBson();
+
+            //Insert1().GetAwaiter().GetResult();
+
+            //Insert2().GetAwaiter().GetResult();
+
+            Insert3().GetAwaiter().GetResult();
+
+            Find().GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Método com exemplo de criação e exibição do documento no console.
+        /// </summary>
+        static void DocumentBson()
+        {
+            var doc = new BsonDocument();
+
+            doc.Add("nome", "joao");
+            doc.Add("idade", 20);
+
+            var array = new BsonArray();
+            array.Add(new BsonDocument("treinamento", "c#"));
+            array.Add(new BsonDocument("treinamento", "mongo"));
+
+            doc.Add("treinamentos", array);
+            //Console.WriteLine(doc);
+
+            //procedimento para capturar elemento.
+            var arrayCapturado = new BsonElement();
+            doc.TryGetElement("treinamentos", out arrayCapturado);
+            Console.WriteLine(arrayCapturado);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Método que retorna um documento criado manualmente.
+        /// </summary>
+        /// <returns></returns>
+        static BsonDocument BsonDocumentBson()
+        {
+            //estancia do BsonDocument
+            var doc = new BsonDocument();
+
+            //Adicinando os dados (chave e valor) do documento
+            doc.Add("nome", "joao");
+            doc.Add("idade", 20);
+
+            //criação de um array para adicionar no documento
+            var array = new BsonArray();
+            array.Add(new BsonDocument("treinamento", "c#"));
+            array.Add(new BsonDocument("treinamento", "mongo"));
+
+            //adicionando o array criado no documento como propriedade (campo) "treinamentos".
+            doc.Add("treinamentos", array);
+
+            //procedimento para capturar elemento.
+            var arrayCapturado = new BsonElement();
+            doc.TryGetElement("treinamentos", out arrayCapturado);
+
+            return doc;
+        }
+
+        /// <summary>
+        /// Método para inclusão de documentos com base no documento criado no métod BsonDocumentBson.
+        /// </summary>
+        /// <returns></returns>
+        static async Task Insert1()
+        {
+            var stringConexao = "mongodb://localhost:27017";
+            var client = new MongoClient(stringConexao);
+
+            var db = client.GetDatabase("treinamentoMongo");
+            var coll = db.GetCollection<BsonDocument>("Pessoa");
+
+            var doc = BsonDocumentBson();
+            await coll.InsertOneAsync(doc);
+        }
+
+        /// <summary>
+        /// Método para inclusão de documento com base na classe Pessoa criado.
+        /// </summary>
+        /// <returns></returns>
+        static async Task Insert2()
+        {
+            var stringConexao = "mongodb://localhost:27017";
+            var client = new MongoClient(stringConexao);
+
+            var db = client.GetDatabase("treinamentoMongo");
+            var coll = db.GetCollection<Pessoa>("Pessoa");
+
+            var pessoa = new Pessoa()
+            {
+                Id = 1,
+                Nome = "Maria",
+                Idade = 18,
+                Treinamentos = new List<string>() { "c#", "MongoDB" }
+            };
+
+            await coll.InsertOneAsync(pessoa);
+        }
+
+        static async Task Insert3()
+        {
+            var stringConexao = "mongodb://localhost:27017";
+            var client = new MongoClient(stringConexao);
+
+            var db = client.GetDatabase("treinamentoMongo");
+            var coll = db.GetCollection<Pessoa2>("Pessoa");
+
+            var pessoa = new Pessoa2()
+            {
+                Codigo = new Random().Next(1, 1000),
+                Salario = 1000,
+                DataInsercao = DateTime.Now
+            };
+
+            await coll.InsertOneAsync(pessoa);
+        }
+
+        /// <summary>
+        /// Metodo para mostrar a data convertida.
+        /// </summary>
+        /// <returns></returns>
+        static async Task Find()
+        {
+            var stringConexao = "mongodb://localhost:27017";
+            var client = new MongoClient(stringConexao);
+
+            var db = client.GetDatabase("treinamentoMongo");
+            var coll = db.GetCollection<Pessoa2>("Pessoa");
+
+            var pessoa = await coll.Find(f => true).ToListAsync();            
+        }
+    }
+}
